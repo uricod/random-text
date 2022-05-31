51b2dc3a890d235d77b64ef536414e3197d23cc6
uri <uri@automationconslt.com>
2022-05-31 13:23:58 -0400

    updated version

--- randomText/__version__.py
+++ randomText/__version__.py
@@ -1,3 +1,3 @@
-__version__ = '1.0.0'
+__version__ = '1.3.0'
 __author__ = 'Josh Nussbaum'
 __email__ = 'joshnuss2@gmail.com'
--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup
 
-from randomText import __author__, __email__, __version__
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='random-text',
-      version=__version__,
+      version='1.3.0',
       description='A Python client library Random Data Api',
       long_description=long_description,
       long_description_content_type='text/markdown; charset=UTF-8',
-      author=__author__,
-      author_email=__email__,
+      author='josh nussbaum',
+      author_email='joshnuss2@gmail.com',
       packages=['randomText', 'randomText.src'],
       url='https://github.com/uricod/random-text',
       include_package_data=True,
@@ -19,7 +17,7 @@ setup(name='random-text',
       license='BSD',
       python_requires='>=3.6',
       install_requires=[
-          'requests',
+          'requests==2.27.1',
           'pandas==1.4.2'
      ],
       classifiers=[
