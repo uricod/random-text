55252c034e3edb2da498ae94044320b4ad54fe5c
uri <uri@automationconslt.com>
2022-05-31 15:26:01 -0400

    examples of product reviews

--- examples.py
+++ examples.py
@@ -14,6 +14,11 @@ def main():
     business_name = client.fancy_random.generate_business_name(size=2)
     print(business_name)
 
+    brand_name = client.fancy_random.generate_brand_name(starting_word='funny')
+    print(brand_name)
+
+    product_reviews = client.fancy_random.generate_product_reviews(size=2, product='toy')
+    print(product_reviews)
 
 if __name__ == '__main__':
     main()
